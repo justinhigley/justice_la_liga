a8e04eb25518b093a81dff7e87cfc1b9fc098d6a
Justin Higley <jhigley@gmail.com>
2022-01-01 10:02:32 -0800

    Create very ugly teams index page

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@themesberg/flowbite", to: "https://ga.jspm.io/npm:@themesberg/flowbite@1.2.0/dist/flowbite.bundle.js"
